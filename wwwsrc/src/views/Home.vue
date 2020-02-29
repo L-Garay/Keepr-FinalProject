b4b726ae644042fbb183f75e4e08cc7bb93a9d0d
Logan Garay <garay.logan@gmail.com>
2020-02-29 12:37:11 -0700

    got rid of video, home is done

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="container-fluid home">
-    <video v-if="modalOpen" autoplay muted loop id="video">
+    <!-- <video v-if="modalOpen" autoplay muted loop id="video">
       <source src="../assets/backgrounds/wallAnimated.mp4" type="video/mp4" />
-    </video>
+    </video>-->
     <div class="row">
-      <div class="col-12 d-flex justify-content-center">
+      <div class="col-12 my-3 d-flex justify-content-center">
         <h1>Welcome To Keepr</h1>
         <button
-          class="btn btn-success"
+          class="btn create"
           type="button"
           data-toggle="modal"
           data-target="#createModal"
-          data-backdrop="false"
           @click="toggleModal"
         >Create a keep</button>
       </div>
@@ -72,11 +71,11 @@
                   >Check this if you want to make it private</label>
                 </div>
                 <div class="buttons">
-                  <button class="btn btn-success" type="submit">Submit Keep</button>
+                  <button class="btn submit" @click="toggleModal" type="submit">Submit Keep</button>
                   <button
                     type="button"
                     @click="toggleModal"
-                    class="btn btn-secondary"
+                    class="btn close2"
                     data-dismiss="modal"
                   >Close</button>
                 </div>
@@ -85,9 +84,9 @@
           </div>
         </div>
       </div>
-      <div class="card-columns">
-        <keep v-for="keep in publicKeeps" :key="keep.id" :keepData="keep" />
-      </div>
+    </div>
+    <div class="card-columns">
+      <keep v-for="keep in publicKeeps" :key="keep.id" :keepData="keep" />
     </div>
   </div>
 </template>
@@ -156,6 +155,7 @@ export default {
 h1 {
   font-family: "Press Start 2P";
   color: orange;
+  text-shadow: 1pt 1pt 3pt black;
 }
 .home {
   background-image: url("../assets/backgrounds/darkWall.jpg");
@@ -166,6 +166,10 @@ h1 {
 .form-check-input {
   margin: 12pt 0 0 5pt;
 }
+.form-check-input:hover {
+  margin: 12pt 0 0 5pt;
+  cursor: pointer;
+}
 .form-check-label {
   margin: 7.5pt 0 0 20pt;
 }
@@ -174,16 +178,70 @@ h1 {
   justify-content: space-between;
   margin-top: 10pt;
   padding-top: 10pt;
-  border-top: 1pt solid grey;
+  border-top: 1pt solid white;
 }
-#video {
+.create {
+  border: 1pt solid orange;
+  color: orange;
+  background-color: rgb(49, 49, 49);
+  margin-left: 15pt;
+}
+.create:hover {
+  border: 1pt solid orange;
+  color: orange;
+  background-color: rgb(49, 49, 49);
+  text-shadow: 0 0 5px orange;
+  box-shadow: 0 0 20px orange;
+}
+.close {
+  color: orange;
+}
+.close:hover {
+  color: red;
+  text-shadow: 0 0 15px red;
+}
+.submit {
+  border: 1pt solid orange;
+  background-color: rgba(0, 0, 0, 0.39);
+  color: orange;
+}
+.submit:hover {
+  border: 1pt solid orange;
+  background-color: rgba(0, 0, 0, 0.39);
+  color: orange;
+  text-shadow: 0 0 10px orange;
+  box-shadow: 2pt 2pt 5pt orange;
+}
+.close2 {
+  border: 1pt solid orange;
+  background-color: rgba(0, 0, 0, 0.39);
+  color: orange;
+}
+.close2:hover {
+  border: 1pt solid red;
+  background-color: rgba(0, 0, 0, 0.39);
+  color: red;
+  text-shadow: 0 0 10px red;
+  box-shadow: 2pt 2pt 5pt red;
+}
+.modal-header {
+  background-color: black;
+  color: orange;
+}
+.modal-body {
+  background-color: rgb(46, 46, 46);
+  color: orange;
+}
+.form-control {
+  background-color: grey;
+  border: 1pt solid orange;
+}
+::placeholder {
+  color: white;
+}
+/* #video {
   width: 100vw;
   height: 100vh;
   object-fit: cover;
-
-  /* z-index:; */
-}
-.modal-backdrop.show {
-  opacity: 0;
-}
+} */
 </style>
